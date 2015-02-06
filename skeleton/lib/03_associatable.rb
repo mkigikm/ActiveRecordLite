79a3e827fb62ba5ba9a98e4e856227eaa3535bd3
Matthew Hicks <mkigikm@gmail.com>
2015-02-06 11:58:40 -0500

    adds belongsto/hasmany options classes

--- skeleton/lib/03_associatable.rb
+++ skeleton/lib/03_associatable.rb
@@ -20,13 +20,17 @@ end
 
 class BelongsToOptions < AssocOptions
   def initialize(name, options = {})
-    # ...
+    @foreign_key = options[:foreign_key] || "#{name}_id".to_sym
+    @class_name  = options[:class_name]  || name.camelcase
+    @primary_key = options[:primary_key] || :id
   end
 end
 
 class HasManyOptions < AssocOptions
   def initialize(name, self_class_name, options = {})
-    # ...
+    @foreign_key = options[:foreign_key] || "#{self_class_name.underscore}_id".to_sym
+    @class_name  = options[:class_name]  || name.camelcase.singularize
+    @primary_key = options[:primary_key] || :id
   end
 end
 
