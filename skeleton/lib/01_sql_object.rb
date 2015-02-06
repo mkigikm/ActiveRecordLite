d9a3bc4351be3cef02bdf4b2bd060cc4e94b9af3
Matthew Hicks <mkigikm@gmail.com>
2015-02-06 11:05:39 -0500

    adds initialize for SQLObject

--- skeleton/lib/01_sql_object.rb
+++ skeleton/lib/01_sql_object.rb
@@ -5,22 +5,39 @@ require 'active_support/inflector'
 
 class SQLObject
   def self.columns
-    # ...
+    DBConnection.execute2(<<-SQL).first.map { |col| col.to_sym }
+    SELECT
+      *
+    FROM
+      #{table_name}
+    LIMIT
+      1
+    SQL
   end
 
   def self.finalize!
+    columns.each do |col|
+      define_method(col) { attributes[col] }
+      define_method("#{col}=") { |value| attributes[col] = value }
+    end
   end
 
   def self.table_name=(table_name)
-    # ...
+    @table_name = table_name
   end
 
   def self.table_name
-    # ...
+    @table_name || name.tableize
   end
 
   def self.all
-    # ...
+    query = <<-SQL
+    SELECT
+      #{table_name}.*
+    FROM
+      #{table_name}
+    SQL
+    puts query
   end
 
   def self.parse_all(results)
@@ -32,11 +49,17 @@ class SQLObject
   end
 
   def initialize(params = {})
-    # ...
+    params.each do |attr_name, value|
+      unless self.class.columns.include?(attr_name.to_sym)
+        raise "unknown attribute '#{attr_name}'"
+      end
+
+      send(attr_name.to_s+"=", value)
+    end
   end
 
   def attributes
-    # ...
+    @attributes ||= {}
   end
 
   def attribute_values
